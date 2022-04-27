1c7eaa0c023317cea67f2c96cf78a914147f8cca
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-27 13:48:42 +0100

    settle betty checker

--- _printf.c
+++ _printf.c
@@ -10,10 +10,12 @@ int _printf(const char * const format, ...)
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
 		{"%%", printf_37},
-		/*{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}*/
+		/**
+		 * {"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		 * {"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		 * {"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		 * {"%S", printf_exclusive_string}, {"%p", printf_pointer}
+		 */
 	};
 
 	va_list args;

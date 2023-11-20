fa4cce0bccf8aa9aa0e4df4add1832d17dec12b1
Abdulmuizz98 <hamzatabdulmuizz98@gmail.com>
2023-11-20 16:46:38 +0100

    fix: mem leaks

--- test/main_18.c
+++ test/main_18.c
@@ -11,12 +11,12 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("%zz");
-	len2 = printf("%zz");
+	len = _printf("%K\n");
+	len2 = printf("%K\n");
 	fflush(stdout);
 	if (len != len2)
 	{
-		printf("Lengths differ. %d, %d\n", len, len2);
+		printf("Lengths differ.\n");
 		fflush(stdout);
 		return (1);
 	}
